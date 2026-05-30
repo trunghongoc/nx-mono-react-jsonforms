6a90f95f4da43580f421a6d7f2deebd28f41a65a
Trung Ho Ngoc <trung@Space.local>
2026-05-30 12:55:39 +0700

    Add libs/ui/core

---
+++ libs/ui/core/src/lib/core.tsx
@@ -0,0 +1,11 @@
+import styles from './core.module.scss';
+
+export function NxMonoReactJsonformsCore() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to NxMonoReactJsonformsCore!</h1>
+    </div>
+  );
+}
+
+export default NxMonoReactJsonformsCore;
